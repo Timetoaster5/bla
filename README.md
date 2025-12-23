255787ae763c867c5fa328528cbc6d3dfd7fd9f6
Timetoaster5 <wraththelokijr@gmail.com>
2025-12-23 07:22:01 +0700

    added several comments and ideas and new functions (that currently do nothing)

--- README.md
+++ README.md
@@ -18,3 +18,19 @@ will create file.bla from file
 
 If I do this I would like it to look for bla headers inside of file.bla 
 to detect it should extract rather than looking at the .bla extension.
+
+Add looking at directories and sub-directories 
+
+Make it not just print the contents of a single file (best cat clone in 2025 right here)
+
+currently im thinking do something like this for writing: 
+1. open dir 
+2. read dir
+3. check if each file is directory or normal file
+4. if directory, the contents of that directory need to be added to
+the current directory struct (if possible to do it like that) 
+5. if normal file, append the contents of the file to the output file
+6. if something else probably error?
+7. I also need to add a proper header to each file (including information about
+sub-directories that it was under), actually info about sub-directories should be
+written to the files in step 4.  
--- src/bla.c
+++ src/bla.c
@@ -1,18 +1,55 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <dirent.h>
 
-int main(int argc, char **argv) {
-	printf("input file: %s\n", argv[1]); // change to not print
+int createbla(char *inputdirpath, char *outputfilepath) {
+	DIR *inputdir;
+	struct dirent *dir;
+	FILE *outputfile; 
+	
+	inputdir = opendir(inputdirpath);	
+	if (!inputdir) {
+		perror("opendir");
+		exit(1);
+	}
 
-	FILE *fileptr;
-	char *file = argv[1];
-
-	fileptr = fopen(file, "r");
-	if (fileptr == NULL) {
-		printf("file %s could not be opened\n", argv[1]);
-	} else {
-		printf("file %s COULD be opened\n", argv[1]);
+	while ((dir = readdir(inputdir)) != NULL) {
+		printf("%s\n", dir->d_name);
 	}	
+	closedir(inputdir); 
+	return 0;
+	//opendir *inputdir 
+	//readdir *inputdir
+	//fopen(*outputfile, "w"); 
+	//go through struct item by item
+	//if (filetype == normal) write contents to *outputfile
+	//else if (filetype == dir) {opendir,readdir, check if file or 
+	//dir, if file add header info about directory path & append to *outputfile, 
+	//elseif dir continue opening it and repeat} 
+	//uhm i was originally thinking about a system of adding all 
+	//dir contents to a single struct but im not entirely sure that would work
+}
 
+int extractbla(char *inputfile, char *outputdir) {
+}
+
+//main should just decide if createbla or extractbla should be called
+int main(int argc, char **argv) {
+	FILE *fileptr;
+	char *path = argv[1];
+
+	fileptr = fopen(path, "r");
+	if (fileptr == NULL) {
+		perror("Failed to open file");
+		return 1; 
+	}	
+	
+	int c; 
+	while ((c = fgetc(fileptr)) != EOF) {
+		printf("%c", c); 
+	}
+	
+	fclose(fileptr);	
 	return 0;
 }
+
